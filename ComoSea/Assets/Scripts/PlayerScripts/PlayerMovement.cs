f0d031ae89c95e1c1221be4709ba33c40a30beb2
aaaaitor <aitortxumu@gmail.com>
2023-10-17 01:56:02 +0200

    Updated player movement.
    Updated the way player movement works so it uses the FixedUpdate method and registers last movement vectors so it isn't look laggy and works better generally.

--- ComoSea/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ ComoSea/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,26 +4,77 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    // Variables used for player movement
+    Rigidbody2D playerRigidBody;
+
+    [HideInInspector]
+    public float lastHorizontalVector;
+    [HideInInspector]
+    public float lastVerticalVector;
+    [HideInInspector]
+    public Vector2 movementDirection;
+    [HideInInspector]
+    public Vector2 lastVectorialDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody2D>();
+
+        // Sets a base value to lastVectorialDirection
+        lastVectorialDirection = new Vector2 (1f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        InputManagement();
+    }
+
+    // Update method that doesnt depend on frames
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Function to control player input.
+    /// </summary>
+    void InputManagement()
+    {
+        // Sets horizontal and vertical axles
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        // Sets movement direction
+        movementDirection = new Vector2(moveX, moveY).normalized;
+
+        // Asigns last vectors of movement
+        if (movementDirection.x != 0)
         {
-            transform.position += new Vector3(1, 0, 0);
+            lastHorizontalVector = movementDirection.x;
+            // Last x axis movement direction
+            lastVectorialDirection = new Vector2(lastHorizontalVector, 0f);
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        if (movementDirection.y != 0)
         {
-            transform.position += new Vector3(-1, 0, 0);
+            lastVerticalVector = movementDirection.y;
+            // Last y axis movement direction
+            lastVectorialDirection = new Vector2(0f, lastVerticalVector);
         }
-        else if (Input.GetKeyDown(KeyCode.W))
+        if (movementDirection.x != 0 && movementDirection.y != 0)
         {
-            transform.position += new Vector3(0, 1, 0);
+            // Last diagonal direction
+            lastVectorialDirection = new Vector2(lastHorizontalVector, lastVerticalVector);
         }
+    }
+
+    /// <summary>
+    /// Controls player movement.
+    /// </summary>
+    void Movement()
+    {
+        playerRigidBody.velocity = new Vector2(movementDirection.x * /*player.currentSpeed*/2f, movementDirection.y * /*player.currentSpeed*/0f);
     }
 }
